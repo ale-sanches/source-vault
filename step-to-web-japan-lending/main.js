94211899d48db80c6f8cf17a1682212d6e72e865
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-02-24 16:09:40 +0300

    japan-landing-6-step
    burger-menu added

--- step-to-web-japan-lending/main.js
+++ step-to-web-japan-lending/main.js
@@ -1,10 +1,25 @@
-(function (){
+(function () {
     const header = document.querySelector(".header");
     window.onscroll = () => {
-        if (window.pageYOffset > 50){
+        if (window.pageYOffset > 50) {
             header.classList.add("header_active");
-        }else{
+        } else {
             header.classList.remove("header_active");
         }
     };
+}());
+
+//Menu burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav-active');
+    })
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav-active');
+    })
 }());
