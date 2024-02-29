949b98fb53efcf30e7ea900b28780d5f999f0542
ale-sanches <130606847+ale-sanches@users.noreply.github.com>
2024-02-29 18:17:01 +0300

    weather-app: v.1.0

---
+++ weather-app(js)/main.js
@@ -0,0 +1,31 @@
+const apiKey = "1da73595a47b504c9a36265c88ef5936";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search__input");
+const searchButton = document.querySelector(".search__button");
+
+async function getWeatherInfo(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if (response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        let data = await response.json();
+
+        document.querySelector(".weather__city-name").innerHTML = data.name;
+        document.querySelector(".weather__temp").innerHTML = Math.round(data.main.temp) + " °C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        const weatherImage = document.querySelector(".weather__image");
+        weatherImage.src = "images/" + `${data.weather[0].main}` + ".png";
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+
+    }
+}
+
+searchButton.addEventListener("click", function () {
+    getWeatherInfo(searchBox.value);
+})
