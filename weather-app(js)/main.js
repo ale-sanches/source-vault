68f097bef03a19d696f07240262d1cbfa81cc01f
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-02-29 17:33:37 +0300

    weather-app:stage 3
    error description added

--- weather-app(js)/main.js
+++ weather-app(js)/main.js
@@ -4,16 +4,30 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search__input");
 const searchButton = document.querySelector(".search__button");
 
-async function getWeatherInfo(city){
-    const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
-    let data = await response.json();
+async function getWeatherInfo(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if (response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        let data = await response.json();
 
-    document.querySelector(".weather__city-name").innerHTML = data.name;
-    document.querySelector(".weather__temp").innerHTML = Math.round(data.main.temp) + " °C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        console.log(data);
+        
+        document.querySelector(".weather__city-name").innerHTML = data.name;
+        document.querySelector(".weather__temp").innerHTML = Math.round(data.main.temp) + " °C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        const weatherImage = document.querySelector(".weather__image");
+        weatherImage.src = "images/" + `${data.weather[0].main}` + ".png";
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+
+    }
 }
 
-searchButton.addEventListener("click", function (){
+searchButton.addEventListener("click", function () {
     getWeatherInfo(searchBox.value);
 })
