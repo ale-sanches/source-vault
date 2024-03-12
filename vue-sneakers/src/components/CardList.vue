d13009cbff954438339fffc1cce04ba7bd6b76ef
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-03-12 11:39:16 +0300

    sneakers(vue project): stage 1
    Main page and cart templates

---
+++ vue-sneakers/src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :is-added="true"
+      :isFavorite="false"
+      :onClickAdd="true"
+    />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
